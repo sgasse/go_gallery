ac49d02c2c8d0f5de090ff50aeda408a1e5b3b23
Simon Gasse <sgasse@users.noreply.github.com>
2021-01-24 10:10:47 +0100

    Add thumbnail creation

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ import (
 	"strings"
 	"text/template"
 	"time"
+
+	"github.com/h2non/bimg"
 )
 
 var (
@@ -24,8 +26,16 @@ var (
 	randomize = flag.Bool("randomize", false, "Random shuffle images")
 )
 
-// imgs contains the paths to image files recursively found in `dir`.
-var imgs []string
+type convTask struct {
+	nameTarget *string
+	file       *string
+}
+
+// paths to image files recursively found in `dir`, including absolute, relative and thumbnail paths.
+var absImgs, imgs, thumbs []string
+
+var thumbDir string
+var convChan chan convTask
 
 // restData is the JSON-formatted response send to the JS callback.
 type restData struct {
@@ -42,12 +52,28 @@ type jsReq struct {
 	FirstRow int `json:"FirstRow"`
 }
 
-func parseImgs(inPath string) []string {
-	var imgPaths []string
+func createPreview(file, outdir string) (outfile string, err error) {
+	buffer, err := bimg.Read(file)
+	if err != nil {
+		return
+	}
 
+	newImage, err := bimg.NewImage(buffer).Resize(800, 600)
+	if err != nil {
+		return
+	}
+
+	outfile = filepath.Join(outdir, filepath.Base(file))
+
+	bimg.Write(outfile, newImage)
+	return
+}
+
+func parseImgs(inPath string) (absPaths, imgPaths []string) {
 	selectImgs := func(path string, info os.FileInfo, err error) error {
 		if err == nil {
 			if strings.HasSuffix(path, ".jpg") || strings.HasSuffix(path, ".jpeg") || strings.HasSuffix(path, ".png") {
+				absPaths = append(absPaths, path)
 				imgPaths = append(imgPaths, "imgs/"+strings.TrimPrefix(path, *dir))
 			}
 		}
@@ -58,21 +84,26 @@ func parseImgs(inPath string) []string {
 	if err != nil {
 		log.Fatal(err)
 	}
-	return imgPaths
+	return absPaths, imgPaths
 }
 
-// maskImgView creates a img path array with all images not in view or the prefetch areas masked out.
-func maskImgView(firstRow int) tplData {
-	startIdx := 0
-	endIdx := (firstRow + *rows + *prefetch) * *cols
+func getMaskInds(firstRow, prefetch int) (startIdx, endIdx int) {
+	// startIdx initialized as zero
+	endIdx = (firstRow + *rows + prefetch) * *cols
 
-	if firstRow >= *prefetch {
-		startIdx = (firstRow - *prefetch) * *cols
+	if firstRow >= prefetch {
+		startIdx = (firstRow - prefetch) * *cols
 	}
 
 	if endIdx > len(imgs) {
 		endIdx = len(imgs)
 	}
+	return
+}
+
+// maskImgView creates a img path array with all images not in view or the prefetch areas masked out.
+func maskImgView(firstRow int) tplData {
+	startIdx, endIdx := getMaskInds(firstRow, *prefetch)
 
 	maskedImgs := make([]string, startIdx)
 	maskedImgs = append(maskedImgs, imgs[startIdx:endIdx]...)
@@ -102,6 +133,7 @@ func restGalleryHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	go findToThumbnail(req.FirstRow, convChan)
 	htmlBody := getGalleryHTML(req.FirstRow)
 	rData := restData{GalleryContent: string(htmlBody)}
 
@@ -121,19 +153,54 @@ func galleryHandler(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, data)
 }
 
-func randomizeData(data []string) []string {
+func randomizeData(arr1, arr2 []string) ([]string, []string) {
 	rand.Seed(time.Now().UnixNano())
-	rand.Shuffle(len(data), func(i, j int) { data[i], data[j] = data[j], data[i] })
-	return data
+	rand.Shuffle(len(arr1), func(i, j int) {
+		arr1[i], arr1[j] = arr1[j], arr1[i]
+		arr2[i], arr2[j] = arr2[j], arr2[i]
+	})
+	return arr1, arr2
+}
+
+func findToThumbnail(firstRow int, toConv chan<- convTask) {
+	startIdx, endIdx := getMaskInds(firstRow, *prefetch+2)
+	for i := startIdx; i < endIdx; i++ {
+		if thumbs[i] == "" {
+			toConv <- convTask{&thumbs[i], &absImgs[i]}
+		}
+	}
+}
+
+func thumbnailImgs(toConv <-chan convTask) {
+	for {
+		task := <-toConv
+		outfile, err := createPreview(*task.file, thumbDir)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		*task.nameTarget = outfile
+	}
 }
 
 func main() {
 	flag.Parse()
 
-	imgs = parseImgs(*dir)
+	absImgs, imgs = parseImgs(*dir)
 	if *randomize {
-		imgs = randomizeData(imgs)
+		absImgs, imgs = randomizeData(absImgs, imgs)
 	}
+	thumbs = make([]string, len(absImgs))
+
+	thumbDir = "/tmp/goGalleryThumbs"
+	err := os.Mkdir(thumbDir, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	convChan = make(chan convTask, 8)
+	go findToThumbnail(0, convChan)
+	go thumbnailImgs(convChan)
 
 	mux := http.NewServeMux()
 
